3f42bd4328fb92b50608924ee4c9c0575c4a5a44
mariosoares <mario.soares@gmail.com>
2023-05-15 13:59:04 -0300

    commit 15/05/2023

---
+++ 4-o-que-eh-a-dom.js
@@ -0,0 +1,4 @@
+// capturando a tag <p>, essa tag vai estar disponível nesta constante "text"
+const text = document.querySelector('p');
+text.innerHTML = 'Olá mundo';
+text.style.color = '#f33';
---
+++ teste.js
@@ -0,0 +1,8 @@
+nome = 'Pedro'
+// console.log(`Olá ${nome}, seja bem-vindo!`);
+// console.error('Olá');
+// console.warn('Olá');
+// console.log({nome});
+
+nome = 'mario';
+console.log({nome});
